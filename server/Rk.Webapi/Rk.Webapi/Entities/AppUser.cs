c4fa9e0acb289099a90a9deee027a4126cec74cb
riyad786 <r.islam@id-ware.com>
2023-02-21 20:13:27 +0100

    Added entities files

---
+++ server/Rk.Webapi/Rk.Webapi/Entities/AppUser.cs
@@ -0,0 +1,9 @@
+﻿namespace Rk.Webapi.Entities
+{
+    public class AppUser
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+
+    }
+}
--- server/Rk.Webapi/Rk.Webapi/WeatherForecast.cs
+++ server/Rk.Webapi/Rk.Webapi/WeatherForecast.cs
@@ -8,7 +8,7 @@ namespace Rk.Webapi
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
-        public string? Summary { get; set; }
-        public string? Enabled { get; set; }
+        public string Summary { get; set; }
+        public string Enabled { get; set; }
     }
 }
