1712325e9b0ad358b98383fac142b962c51ead51
khondakar22 <khondakar22readul@gmail.com>
2023-02-22 22:59:23 +0100

    Add SSL Certificate

--- client/rk-client/src/app/app.module.ts
+++ client/rk-client/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { FormsModule } from '@angular/forms';
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- server/Rk.Webapi/Rk.Webapi/Program.cs
+++ server/Rk.Webapi/Rk.Webapi/Program.cs
@@ -17,9 +17,10 @@ builder.Services.AddCors();
 
 var app = builder.Build();
 
-app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 // Configure the HTTP request pipeline.
 
 app.MapControllers();
 
 app.Run();
+ 
