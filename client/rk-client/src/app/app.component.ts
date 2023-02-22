a4302c2f6eb734099e416b83adf81c1a6a1ffb0a
khondakar22 <khondakar22readul@gmail.com>
2023-02-22 22:08:27 +0100

    Add Cors

--- client/rk-client/src/app/app.component.ts
+++ client/rk-client/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'rk-client';
+  users: Array<any> = [];
+  constructor(private http: HttpClient) {}
+  ngOnInit(): void {
+    this.http
+      .get(' https://localhost:5001/api/users')
+      .subscribe({
+        next: (respose: any) => this.users.push(...respose),
+        error: (error: any) => console.log(error),
+        complete: () => console.log('complete'),
+      });
+  }
 }
