71f9b539ee23d95a54a870db6ba4bf3d16ade596
khondakar22 <khondakar22readul@gmail.com>
2023-02-23 22:39:58 +0100

    Added Dto

--- server/Rk.Webapi/Rk.Webapi/Controllers/AccountController.cs
+++ server/Rk.Webapi/Rk.Webapi/Controllers/AccountController.cs
@@ -1,7 +1,9 @@
 ﻿using System.Security.Cryptography;
 using System.Text;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Rk.Webapi.Data;
+using Rk.Webapi.Dto;
 using Rk.Webapi.Entities;
 
 namespace Rk.Webapi.Controllers
@@ -16,18 +18,24 @@ namespace Rk.Webapi.Controllers
         }
 
         [HttpPost("register")] // POST: api/account/register
-        public async Task<ActionResult<AppUser>> Register(string username, string password)
+        public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
         {
+            if (await UserExists(registerDto.Username)) return BadRequest("Username is taken");
             using var hmac = new HMACSHA512();
             var user = new AppUser
             {
-                UserName = username,
-                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+                UserName = registerDto.Username.ToLower(),
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
                 PasswordSaltBytes = hmac.Key
             };
             _context.Users.Add(user);
             await _context.SaveChangesAsync();
             return Ok(user);
+        }
+
+        private async Task<bool> UserExists(string username)
+        {
+            return await _context.Users.AnyAsync(x => x.UserName.ToLower() == username.ToLower());
         }
     }
 }
---
+++ server/Rk.Webapi/Rk.Webapi/Dto/RegisterDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Rk.Webapi.Dto
+{
+    public class RegisterDto
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+    }
+}
