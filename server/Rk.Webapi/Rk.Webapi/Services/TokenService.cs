627b4d8666c284b8c2554eb10c6ee4c34462a888
khondakar22 <khondakar22readul@gmail.com>
2023-02-24 21:52:59 +0100

    Added the token logics

--- server/Rk.Webapi/Rk.Webapi/Services/TokenService.cs
+++ server/Rk.Webapi/Rk.Webapi/Services/TokenService.cs
@@ -1,13 +1,36 @@
-﻿using Rk.Webapi.Entities;
+﻿using Microsoft.IdentityModel.Tokens;
+using Rk.Webapi.Entities;
 using Rk.Webapi.Interfaces;
-
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 namespace Rk.Webapi.Services
 {
     public class TokenService : ITokenService
     {
+        private readonly SymmetricSecurityKey _key;
+        public TokenService(IConfiguration config)
+        {
+            _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"]));
+        }
         public string CrateToken(AppUser appUser)
         {
-            throw new NotImplementedException();
+            var claims = new List<Claim>
+            {
+                 new Claim(JwtRegisteredClaimNames.NameId, appUser.UserName)
+            };
+            var creds = new SigningCredentials(_key, SecurityAlgorithms.HmacSha256Signature);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(7),
+                SigningCredentials = creds
+            };
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+
+               
         }
     }
 }
