0107c3438475a3d620e34aa074f38a8e971fd55b
khondakar22 <khondakar22readul@gmail.com>
2023-02-26 23:04:18 +0100

    Added some more themes

--- client/rk-client/src/app/app-routing.module.ts
+++ client/rk-client/src/app/app-routing.module.ts
@@ -2,12 +2,20 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
+import { MemberDetailsComponent } from './members/member-details/member-details.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
 
 const routes: Routes = [
-  
+  { path: '',   redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent  },
   { path: 'login', component: LoginComponent  },
-  { path: '',   redirectTo: '/home', pathMatch: 'full' },
+  { path: 'members', component: MemberListComponent },
+  { path: 'members/:id', component: MemberDetailsComponent  },
+  { path: 'lists', component: ListsComponent  },
+  { path: 'messages', component: MessagesComponent  },
+  { path: '**',   component: HomeComponent, pathMatch: 'full' },
 ];
 
 @NgModule({
