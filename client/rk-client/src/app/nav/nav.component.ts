7e2b890d439e0b8c511fbe7ce6b9c7deaef33f3a
khondakar22 <khondakar22readul@gmail.com>
2023-02-26 19:56:16 +0100

    Used services in html

--- client/rk-client/src/app/nav/nav.component.ts
+++ client/rk-client/src/app/nav/nav.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
 import { AccountService } from '../_services/account.service';
-import { Observable, Subscription, of } from 'rxjs';
-import { User } from '../_models/user';
+// import { Observable, Subscription, of } from 'rxjs';
+// import { User } from '../_models/user';
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
@@ -10,11 +10,11 @@ import { User } from '../_models/user';
 })
 export class NavComponent implements OnInit, OnDestroy {
   model: any = {};
-  currentUser$: Observable<User | null> = of(null);
-  constructor(public router: Router, private accountService: AccountService) {}
+  // currentUser$: Observable<User | null> = of(null);
+  constructor(public router: Router, public accountService: AccountService) {}
   ngOnDestroy(): void {}
   ngOnInit(): void {
-    this.currentUser$ = this.accountService.currentUser$;
+    // this.currentUser$ = this.accountService.currentUser$;
   }
 
   onLogout() {
