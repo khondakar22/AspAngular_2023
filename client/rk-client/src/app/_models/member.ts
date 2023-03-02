a58acfc0d833e088676085870215d88dd74f9d90
khondakar22 <khondakar22readul@gmail.com>
2023-03-02 15:57:19 +0100

    Added two model in client

---
+++ client/rk-client/src/app/_models/member.ts
@@ -0,0 +1,18 @@
+import { Photo } from "./photo";
+
+export interface RootObject {
+  id: number;
+  userName: string;
+  photoUrl: string;
+  dateOfBirth: Date;
+  knownAs: string;
+  created: Date;
+  lastActive: Date;
+  age: number;
+  introduction: string;
+  lookingFor: string;
+  interests: string;
+  city: string;
+  country: string;
+  photos: Photo[];
+}
