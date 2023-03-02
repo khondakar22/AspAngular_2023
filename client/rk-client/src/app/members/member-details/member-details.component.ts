e0018ba5d0e408259b53d5911556665035bc3a99
khondakar22 <khondakar22readul@gmail.com>
2023-03-02 23:18:50 +0100

    Added member details pages

--- client/rk-client/src/app/members/member-details/member-details.component.ts
+++ client/rk-client/src/app/members/member-details/member-details.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Member } from 'src/app/_models/member';
+import { MembersService } from 'src/app/_services/members.service';
 
 @Component({
   selector: 'app-member-details',
   templateUrl: './member-details.component.html',
   styleUrls: ['./member-details.component.css']
 })
-export class MemberDetailsComponent {
+export class MemberDetailsComponent implements OnInit {
+  member: Member | undefined;
+  constructor(private memberService: MembersService, private route: ActivatedRoute){}
+  ngOnInit(): void {
+    this.loadMember();
+  }
+
+  loadMember(){
+    const username = this.route.snapshot.paramMap.get('userName');
+    if(!username) return;
+    this.memberService.getMember(username).subscribe({
+      next: res => this.member = res
+    })
+  }
 
 }
