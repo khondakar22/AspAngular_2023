a7df6998a33a2a2c70112b63ec848ec3c8d4466a
khondakar22 <khondakar22readul@gmail.com>
2023-03-03 21:13:42 +0100

    Added Member edit

--- client/rk-client/src/app/members/member-edit/member-edit.component.ts
+++ client/rk-client/src/app/members/member-edit/member-edit.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { take } from 'rxjs';
+import { Member } from 'src/app/_models/member';
+import { User } from 'src/app/_models/user';
+import { AccountService } from 'src/app/_services/account.service';
+import { MembersService } from 'src/app/_services/members.service';
 
 @Component({
   selector: 'app-member-edit',
   templateUrl: './member-edit.component.html',
   styleUrls: ['./member-edit.component.css']
 })
-export class MemberEditComponent {
+export class MemberEditComponent implements OnInit {
+  member: Member | undefined;
+  user: User | null = null;
+
+  constructor(private acoountService: AccountService, private memberService: MembersService){
+    this.acoountService.currentUser$.pipe(take(1)).subscribe({
+      next: user => this.user = user
+    })
+  }
+  ngOnInit(): void {
+    this.loadMember();
+  }
+
+  loadMember() {
+    if(!this.user)return;
+    this.memberService.getMember(this.user.username).subscribe({
+      next: res => this.member = res
+    })
+  }
+
+
 
 }
